ed3f0a700c4c0a99e167dbfa00661cb7c6dda2af
TheNoxNox <77751858+TheNoxNox@users.noreply.github.com>
2021-04-23 00:57:21 -0400

    Number adjustments

--- Assets/Scripts/Player/States/AttackStates/PlayerAState_Idle.cs
+++ Assets/Scripts/Player/States/AttackStates/PlayerAState_Idle.cs
@@ -27,11 +27,6 @@ public class PlayerAState_Idle : PlayerAState
                 myStateMachine.ThePlayerPawn.Owner
                 );
 
-            //if (!myStateMachine.ThePlayerPawn.IsFacingRight())
-            //{
-            //    attackDir.x *= -1f;
-            //}
-
             attackHitbox.transform.localPosition = attackDir;
             attackHitbox.transform.localRotation = updownRotation;
 
