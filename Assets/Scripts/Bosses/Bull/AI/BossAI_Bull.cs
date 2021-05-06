40edd9f48f1ad14cc8004ff3c3636c8ab73cf470
runnerdude127 <runnerdude127@gmail.com>
2021-05-05 21:35:32 -0400

    Bull AI Basis
    The base for Bull's AI has been created :)

--- Assets/Scripts/Bosses/Bull/AI/BossAI_Bull.cs
+++ Assets/Scripts/Bosses/Bull/AI/BossAI_Bull.cs
@@ -11,15 +11,79 @@ public class BossAI_Bull : BossAI
     /// </summary>
     public BullController bull;
 
+    bool pinchMode = false;
+    bool attackCooldown = false;
+    int currentAttack = 1;
+
     private void Awake()
     {
         // The AttackCycleFinish event is called whenever Bull finishes one of his Sing, Charge, or Jump attacks.
         // Here is an example of adding some command after Bull finishes his current attack pattern.
-        bull.theBullPawn.AttackCycleFinish += AttackPatternExample;
+        if (bull.theBullPawn)
+        {
+            bull.theBullPawn.AttackCycleFinish += StartAttackCycle;
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (bull.theBullPawn.CurrentHealth < bull.theBullPawn.GetActorMaxHealth() / 2)
+        {
+            pinchMode = true;
+        }
+        else
+        {
+            pinchMode = false;
+        }
+    }
+
+    public void StartAttackCycle()
+    {
+        StartCoroutine(AttackCycle());
+        bull.theBullPawn.AttackCycleFinish -= StartAttackCycle;
+    }
+
+    public virtual IEnumerator AttackCycle()
+    {
+        int attackChance;
+        attackCooldown = true;
+
+        if (pinchMode == true)
+        {
+            attackChance = Random.Range(1, 3);
+        }
+        else
+        {
+            attackChance = currentAttack;
+        }
+
+        if (bull.theBullPawn.OwnStateMachine.CurrentAttackState is BullAState_Idle)
+        {
+            if (attackChance == 1)
+            {
+                bull.DoAttack2(GameInstanceManager.Main.ThePlayer.Location);             
+            }
+            else if (attackChance == 2)
+            {
+                bull.DoAttack1(GameInstanceManager.Main.ThePlayer.Location);
+            }
+            else if (attackChance == 3)
+            {
+                bull.DoAttack3(GameInstanceManager.Main.ThePlayer.Location);
+            }
+        }
+        yield return new WaitUntil (() => bull.theBullPawn.OwnStateMachine.CurrentAttackState is BullAState_Idle);
+        attackCooldown = false;
+        currentAttack++;
+        if (currentAttack > 3)
+        {
+            currentAttack = 1;
+        }
     }
 
     public void AttackPatternExample()
-    {
+    {       
         // fulfill conditions here.
         if (exampleCondition)
         {
